fabceede4c74ba8700f94a7529f4a1d415831a12
Joaquín <Joaquin.deMarjal@gmail.com>
2026-08-08 14:20:10 +0200

    Clean up legacy frontend architecture

--- frontend/src/actions/gameActions.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import { redirect } from "next/navigation";
-
-export async function searchGames(formData: FormData) {
-  const query = formData.get("query");
-
-  redirect(`/?q=${query}`);
-}
